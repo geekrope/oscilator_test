c205cec1ac240c1d8a4fff789cdd8d5b8220f3d3
geekrope <geekrope@gmail.com>
2023-09-10 17:44:40 +0400

    Добавьте файлы проекта.

---
+++ oscilator_test/app.js
@@ -0,0 +1,85 @@
+window.onclick = function () {
+    var audioContext = new AudioContext();
+    var baseNote = Note.Base.Transpose(3, 0);
+    var fmNote = baseNote.Transpose(0, 2);
+    var oscilator = audioContext.createOscillator();
+    var fmDepth = 300;
+    oscilator.frequency.setValueAtTime(baseNote.Frequency, audioContext.currentTime);
+    oscilator.type = "sine";
+    function fmWarp(t, depth, frequency) {
+        return baseNote.Frequency + Math.sin(t * frequency * Math.PI) * depth;
+    }
+    var fm = [];
+    var duration = 10;
+    for (var t = 0; t < duration; t += 1 / audioContext.sampleRate) {
+        fm.push(fmWarp(t, fmDepth, fmNote.Frequency));
+    }
+    oscilator.frequency.setValueCurveAtTime(fm, audioContext.currentTime, duration);
+    oscilator.connect(audioContext.destination);
+    oscilator.start();
+};
+var KeysScale;
+(function (KeysScale) {
+    KeysScale[KeysScale["A"] = 0] = "A";
+    KeysScale[KeysScale["A#"] = 1] = "A#";
+    KeysScale[KeysScale["B"] = 2] = "B";
+    KeysScale[KeysScale["C"] = 3] = "C";
+    KeysScale[KeysScale["C#"] = 4] = "C#";
+    KeysScale[KeysScale["D"] = 5] = "D";
+    KeysScale[KeysScale["D#"] = 6] = "D#";
+    KeysScale[KeysScale["E"] = 7] = "E";
+    KeysScale[KeysScale["F"] = 8] = "F";
+    KeysScale[KeysScale["F#"] = 9] = "F#";
+    KeysScale[KeysScale["G"] = 10] = "G";
+    KeysScale[KeysScale["G#"] = 11] = "G#";
+})(KeysScale || (KeysScale = {}));
+var Settings = /** @class */ (function () {
+    function Settings() {
+    }
+    Object.defineProperty(Settings, "OctaveLength", {
+        get: function () {
+            return 12;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Settings;
+}());
+var Note = /** @class */ (function () {
+    function Note(key, octave, frequency) {
+        this._key = key;
+        this._octave = octave;
+        this._frequency = frequency;
+    }
+    Object.defineProperty(Note.prototype, "Key", {
+        get: function () {
+            return this._key;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Note.prototype, "Octave", {
+        get: function () {
+            return this._octave;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Note.prototype, "Frequency", {
+        get: function () {
+            return this._frequency;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Note.prototype.Transpose = function (keys, octaves) {
+        var key = (keys + this.Key) % Settings.OctaveLength;
+        var octave = (octaves + this.Octave);
+        var transposeLength = keys + octaves * Settings.OctaveLength;
+        var frequency = this.Frequency * Math.pow(2, transposeLength / 12);
+        return new Note(key, octave, frequency);
+    };
+    Note.Base = new Note(KeysScale.A, 0, 55);
+    return Note;
+}());
+//# sourceMappingURL=app.js.map
---
+++ oscilator_test/app.ts
@@ -0,0 +1,92 @@
+﻿window.onclick = () =>
+{
+	const audioContext = new AudioContext();
+	const baseNote = Note.Base.Transpose(3, 0);
+	const fmNote = baseNote.Transpose(0, 2);
+	const oscilator = audioContext.createOscillator();
+	let fmDepth = 300;
+
+	oscilator.frequency.setValueAtTime(baseNote.Frequency, audioContext.currentTime);
+	oscilator.type = "sine";
+
+	function fmWarp(t: number, depth: number, frequency: number)
+	{
+		return baseNote.Frequency + Math.sin(t * frequency * Math.PI) * depth;
+	}
+
+	const fm = [];
+	const duration = 10;
+
+	for (let t = 0; t < duration; t += 1 / audioContext.sampleRate)
+	{
+		fm.push(fmWarp(t, fmDepth, fmNote.Frequency));
+	}
+
+	oscilator.frequency.setValueCurveAtTime(fm, audioContext.currentTime, duration)
+
+	oscilator.connect(audioContext.destination);
+	oscilator.start();
+}
+
+enum KeysScale
+{
+	"A" = 0,
+	"A#" = 1,
+	"B" = 2,
+	"C" = 3,
+	"C#" = 4,
+	"D" = 5,
+	"D#" = 6,
+	"E" = 7,
+	"F" = 8,
+	"F#" = 9,
+	"G" = 10,
+	"G#" = 11
+}
+
+class Settings
+{
+	public static get OctaveLength(): number
+	{
+		return 12;
+	}
+}
+
+class Note
+{
+	private _key: KeysScale;
+	private _octave: number;
+	private _frequency: number;
+
+	public static Base = new Note(KeysScale.A, 0, 55);
+
+	public get Key()
+	{
+		return this._key;
+	}
+	public get Octave()
+	{
+		return this._octave;
+	}
+	public get Frequency()
+	{
+		return this._frequency;
+	}
+
+	public Transpose(keys: number, octaves: number)
+	{
+		const key = (keys + this.Key) % Settings.OctaveLength as KeysScale;
+		const octave = (octaves + this.Octave);
+		const transposeLength = keys + octaves * Settings.OctaveLength;
+		const frequency = this.Frequency * Math.pow(2, transposeLength / 12);
+
+		return new Note(key, octave, frequency);
+	}
+
+	private constructor(key: KeysScale, octave: number, frequency: number)
+	{
+		this._key = key;
+		this._octave = octave;
+		this._frequency = frequency;
+	}
+}
